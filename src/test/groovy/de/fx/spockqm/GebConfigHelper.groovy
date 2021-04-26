0837ca3a127bf9bf549dc6823715cd4f682054ca
felix 10sing <f.tensing@gmail.com>
2021-04-26 10:52:13 +0200

    * initHeadless

--- src/test/groovy/de/fx/spockqm/GebConfigHelper.groovy
+++ src/test/groovy/de/fx/spockqm/GebConfigHelper.groovy
@@ -7,13 +7,21 @@ import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.firefox.FirefoxOptions
 /**
- * Helper for creating Webdriver.
+ * Helper for creating GebConfig.
  * Helper is easy to Debug (GebConfig is not debuggable).
- * Default:
- *   driver: Chrome
- *   headless: false
  */
 class GebConfigHelper {
+
+    /**
+     * Checks if System-Property is set, if not sets the default value as System-Property.
+     * @param defaultValue default-value to use
+     */
+    static initHeadless(boolean defaultValue){
+        def headless = System.getProperty("headless")
+        if(headless==null){
+            System.setProperty("headless",defaultValue.toString())
+        }
+    }
 
     /**
      * Creates Driver-Specific Reports-Directory, and sets spock-reports System-Property for this directory.
@@ -21,17 +29,18 @@ class GebConfigHelper {
     static handleReportsDir(){
         def reportsDir = 'target/spock-reports/'+System.getenv("driver.active")
         System.setProperty("com.athaydes.spockframework.report.outputDir",reportsDir)
-        println ("***** $reportsDir")
         return  reportsDir
     }
     /**
      * Creates Webdriver.
      * System-Properties that are read:
-     *   driver: the driver to use. Supported Values are 'chrome' and 'firefox'.
+     *   driver.active: the driver to use. Supported Values are 'chrome' and 'firefox'.
+     *   If no driver is set, chrome is used as default.
+     *   driver.headless: defines if the test is executes headless
      */
     static createDriver() {
         def activeDriver = Objects.requireNonNullElse(System.getenv("driver.active"),"default")
-        def headless = Boolean.valueOf(System.getProperty("driver.headless", "false"))
+        def headless = Boolean.valueOf(System.getProperty("driver.headless"))
         def webdriver
         switch (activeDriver) {
             case "firefox":
--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -1,11 +1,12 @@
-import geb.Browser
-import geb.Page
-import geb.PageEventListenerSupport
-
 import static de.fx.spockqm.GebConfigHelper.createDriver
 import static de.fx.spockqm.GebConfigHelper.handleReportsDir
+import static de.fx.spockqm.GebConfigHelper.initHeadless
+
+
+initHeadless(true)
 
 reportsDir = handleReportsDir()
+
 baseUrl = "https://qualityminds.de/"
 
 driver = { createDriver() }
